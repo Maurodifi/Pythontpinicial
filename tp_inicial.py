c4a6a9f239b7c3e8b6fcb3b08a1b70bd1720c261
maurodifi <maurodifi16@gmail.com>
2023-09-18 11:49:55 -0300

    Corrigo treeview y agrego regex

--- tp_inicial.py
+++ tp_inicial.py
@@ -3,12 +3,13 @@ from tkinter.messagebox import *
 from tkinter import ttk
 import tkinter as tk
 import sqlite3
+import re
 
 mi_id = 0
 main = Tk()
 main.title("System UTNBA")
 main.configure(bg='#F0F7DA')
-titulo = Label(main, text="System UTNBA", width=80, foreground='#F0F7DA', bg='#1F192F', font='Arial 15 bold').grid(pady=10, row=0, columnspan=5)
+titulo = Label(main, text="System UTNBA", width=80, foreground='#F0F7DA', bg='#1F192F', font='Arial 15 bold').grid(pady=10, row=0, columnspan=6)
 fuentetit = 'Arial 11'
 fuentecue = 'Arial 10'
 
@@ -20,15 +21,22 @@ db.commit()
 
 def regpersona():
     global mi_id
-    mi_id += 1
-    sql = "INSERT INTO personas (nombre, apellido, documento, correo, filiacion) VALUES (?,?,?,?,?)"
-    datos = (var_nombre.get(),var_apellido.get(),var_dni.get(),var_correo.get(),combo.get())
-    cursor.execute(sql, datos)
-    db.commit()
-    tree.insert("", "end", text=str(mi_id),values=(var_nombre.get(), var_apellido.get(),var_dni.get(),var_correo.get(),combo.get()))
+    patron = "^[A-Za-z]+(?i:[ _-][A-Za-z]+)*$"
+    patrondni = "^[0-9]+(?i:[ _-][0-9]+)*$"
+    if (re.match(patron, var_nombre.get()) and re.match(patrondni, var_dni.get()) and re.match(patron, var_apellido.get())):
+        mi_id += 1
+        sql = "INSERT INTO personas (nombre, apellido, documento, correo, filiacion) VALUES (?,?,?,?,?)"
+        datos = (var_nombre.get(),var_apellido.get(),var_dni.get(),var_correo.get(),combo.get())
+        cursor.execute(sql, datos)
+        db.commit()
+        mostrardb()
+    else:
+        showerror("Error", "No es posible guardar")
+   
 
 def conspersona():
-    if var_dnic.get() != "":
+    patrondni = "^[0-9]+(?i:[ _-][0-9]+)*$"
+    if (re.match(patrondni, var_dnic.get())):
         sql = "SELECT * FROM personas WHERE documento = " + var_dnic.get()
         cursor.execute(sql)
         resultado = cursor.fetchall()
@@ -37,7 +45,7 @@ def conspersona():
         else:
             showerror("Error", "No existe en la base de datos")
     else:
-        showwarning("Error", "Por favor ingrese un valor numerico")
+        showwarning("Error", "Por favor ingrese un valor valido")
 
 
 def modpersona():
@@ -55,6 +63,7 @@ def eliminarpersona():
         if askyesno("Eliminar persona", "Esta seguro que desea eliminar esta persona"):
             sql = "DELETE FROM personas WHERE documento =" + var_dnic.get()
             cursor.execute(sql)
+            mostrardb()
             showinfo("Eliminar", "Usuario eliminado")
         else:
             showinfo("Salir", "Esta a punto de salir")
@@ -69,29 +78,29 @@ var_dni = StringVar()
 var_correo = StringVar()
 var_dnic = StringVar()
 # ---------- ALTA DE REGISTRO ---------------------------
-registrasetitulo = Label(main, text="Registrarse", bg='#ced4db', font=fuentetit).grid(pady=10,row=1, column=0, columnspan=2)
-nombre = Label(main, text="Nombre", bg='#F0F7DA', font=fuentecue).grid(column=0, row=2)
-nombreentry = Entry(main, textvariable=var_nombre).grid(column=1, row=2)
+registrasetitulo = Label(main, text="Registrarse", bg='#ced4db', font=fuentetit).grid(pady=10,row=1, column=1, columnspan=2)
+nombre = Label(main, text="Nombre", bg='#F0F7DA', font=fuentecue).grid(column=1, row=2)
+nombreentry = Entry(main, textvariable=var_nombre).grid(column=2, row=2)
 
-apellido = Label(main, text="Apellido", bg='#F0F7DA', font=fuentecue).grid(column=0, row=3)
-apellidoentry = Entry(main,textvariable=var_apellido).grid(column=1, row=3)
+apellido = Label(main, text="Apellido", bg='#F0F7DA', font=fuentecue).grid(column=1, row=3)
+apellidoentry = Entry(main,textvariable=var_apellido).grid(column=2, row=3)
 
-dni = Label(main, text="Dni", bg='#F0F7DA', font=fuentecue).grid(column=0, row=4)
-dnientry = Entry(main,textvariable=var_dni).grid(column=1, row=4)
+dni = Label(main, text="Dni", bg='#F0F7DA', font=fuentecue).grid(column=1, row=4)
+dnientry = Entry(main,textvariable=var_dni).grid(column=2, row=4)
 
-correo = Label(main, text="Correo", bg='#F0F7DA', font=fuentecue).grid(column=0, row=5)
-correoentry = Entry(main,textvariable=var_correo).grid(column=1, row=5)
+correo = Label(main, text="Correo", bg='#F0F7DA', font=fuentecue).grid(column=1, row=5)
+correoentry = Entry(main,textvariable=var_correo).grid(column=2, row=5)
 
-filiacion = Label(main, text="Filiacion",bg='#F0F7DA', font=fuentecue).grid(column=0, row=6)
+filiacion = Label(main, text="Filiacion",bg='#F0F7DA', font=fuentecue).grid(column=1, row=6, pady=3)
 #filiacionentry = Entry(main,textvariable=var_filiacion).grid(column=1, row=6)
 
-boton_reg = Button(main, text="Registrar", bg='#65B8A6', command=regpersona).grid(pady=10, row=7, column=0, columnspan=2)
+boton_reg = Button(main, text="Registrar", bg='#65B8A6', command=regpersona).grid(pady=10, row=7, column=1, columnspan=2)
 
 combo = ttk.Combobox(
     state="readonly",
     values=["Docente", "No Docente", "Alumno", "Monotributista"]
 )
-combo.grid(column=1,row=6)
+combo.grid(column=2,row=6)
 
 """
 ------------ CONSULTA Y EDITAR -------------------
@@ -112,34 +121,41 @@ filiacionc = Label(main, text="Filiacion", bg='#F0F7DA', font=fuentecue).grid(co
 filiacioncentry = Entry(main).grid(column=4, row=6)
 """
 # INPUT MODIFICACION/CONSULTA
-consultatitulo = Label(main, text="Consulta", bg='#ced4db', font=fuentetit).grid(pady=10, row=1, column=2, columnspan=3)
-dniconsulta = Label(main, text="Ingrese Documento", bg='#F0F7DA', font=fuentecue).grid(pady=10, column=2, row=2)
+consultatitulo = Label(main, text="Consulta", bg='#ced4db', font=fuentetit).grid(pady=10, row=1, column=3, columnspan=2)
+dniconsulta = Label(main, text="Ingrese Documento", bg='#F0F7DA', font=fuentecue).grid(column=3, row=2)
 dniconsultaentry = Entry(main, textvariable=var_dnic).grid(column=4, row=2 )
 
-boton_mod = Button(main, text="Consultar", bg='#2d6073', command=conspersona).grid(column=2, row=3)
-boton_con = Button(main, text="Modificar", bg='#65B8A6', command=modpersona).grid(row=3, column=3)
+boton_con = Button(main, text="Consultar", bg='#2d6073', command=conspersona).grid(column=3, row=4, columnspan=2)
+boton_mod = Button(main, text="Modificar", bg='#65B8A6', command=modpersona).grid(row=3, column=3)
 boton_eli = Button(main, text="Eliminar", bg='#ced4db', command=eliminarpersona).grid(row=3, column=4)
 
 # MUESTRA DE BASE
 
-tree = ttk.Treeview(main)
-tree["columns"] = ("col1", "col2", "col3", "col4", "col5")
-tree.column("#0", minwidth=40, anchor="n")  # no tiene encabezado porque ya existe
-tree.column("col1", minwidth=80, anchor="n")
-tree.column("col2", minwidth=80, anchor="n")
-tree.column("col3", minwidth=60, anchor="n")
-tree.column("col4", minwidth=60, anchor="n")
-tree.column("col5", minwidth=60, anchor="n")
+tree = ttk.Treeview(main, show="headings")
+tree["columns"] = ("1", "2", "3", "4", "5", "6")
+tree.column("1", minwidth=40, anchor="n")  # no tiene encabezado porque ya existe
+tree.column("2", minwidth=80, anchor="n")
+tree.column("3", minwidth=80, anchor="n")
+tree.column("4", minwidth=60, anchor="n")
+tree.column("5", minwidth=60, anchor="n")
+tree.column("6", minwidth=60, anchor="n")
 
 # Encabezado de las columnas del Treeview
-tree.heading("#0", text="ID", anchor="n")
-tree.heading("col1", text="Nombre", anchor="n")
-tree.heading("col2", text="Apellido", anchor="n")
-tree.heading("col3", text="Dni", anchor="n")
-tree.heading("col4", text="Correo", anchor="n")
-tree.heading("col5", text="Filiacion", anchor="n")
+tree.heading("1", text="ID", anchor="n")
+tree.heading("2", text="Nombre", anchor="n")
+tree.heading("3", text="Apellido", anchor="n")
+tree.heading("4", text="Dni", anchor="n")
+tree.heading("5", text="Correo", anchor="n")
+tree.heading("6", text="Filiacion", anchor="n")
 
-tree.grid(row=9, columnspan=5, pady=5)
+def mostrardb():
+    sql = "SELECT * FROM personas"
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+    for i in resultado:
+        tree.insert("", 'end', values=i)
 
+tree.grid(row=9, columnspan=6, pady=5)
+mostrardb()
 
 main.mainloop()
